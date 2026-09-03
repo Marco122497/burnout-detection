0217b38462460b07c87145638812e5807f9ea63d
Dether John Gorre <blueblird9724@gmail.com>
2026-09-03 16:44:35 +0800

    footer to MARCO

--- lib/app-meta.ts
+++ lib/app-meta.ts
@@ -2,7 +2,7 @@ export const APP_VERSION = "1.1.1.24-beta";
 
 export const APP_COMPANY = "MarcoD Solutions, Inc.";
 
-export const APP_DEVELOPER = "Dether John Gorre";
+export const APP_DEVELOPER = "M A R C O";
 
 export const APP_BUILD_TIME =
   process.env.NEXT_PUBLIC_BUILD_TIME ?? new Date().toISOString();
